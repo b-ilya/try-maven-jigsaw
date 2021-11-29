f75b1dd1211fa3711ee043ddecf25a096d8234d2
b-ilya <b.ilya.work@gmail.com>
2021-11-29 04:36:32 +0200

    Added bundle module to make jlinked package

---
+++ bundle/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module dev.vinca.learn.mavenjigsaw.bundle {
+    requires dev.vinca.learn.mavenjigsaw.user;
+    
+}
--- user/src/module-info.java
+++ user/src/module-info.java
@@ -1,3 +1,5 @@
 module dev.vinca.learn.mavenjigsaw.user {
     requires dev.vinca.learn.mavenjigsaw.spi;
+
+    exports dev.vinca.learn.mavenjigsaw.user;
 }
